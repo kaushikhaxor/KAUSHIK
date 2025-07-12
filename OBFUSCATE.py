47a5c2b26b3dd1b77f9d97fb3bb49b8d14ee4227
乛KAUSHIK๛ㅤ <143575723+kaushikhaxor@users.noreply.github.com>
2025-07-12 23:27:03 +0530

    Add files via upload

---
+++ OBFUSCATE.py
@@ -0,0 +1,253 @@
+oipTZkHt = __import__((lambda x, s: (lambda x, y: ''.join(chr(q ^ x) for q in
+    y))(103, []).join(chr(c ^ x) for c in s))(247, [152, 132]))
+ofJEKnJy = __import__((lambda x, s: (lambda x, y: ''.join(chr(q ^ x) for q in
+    y))(42, []).join(chr(c ^ x) for c in s))(247, [154, 150, 133, 132, 159,
+    150, 155]))
+AFBgiaeo = __import__((lambda x, s: (lambda x, y: ''.join(chr(q ^ x) for q in
+    y))(237, []).join(chr(c ^ x) for c in s))(247, [150, 132, 131, 152, 133]))
+PbItsxAJ = __import__((lambda x, s: (lambda x, y: ''.join(chr(q ^ x) for q in
+    y))(87, []).join(chr(c ^ x) for c in s))(247, [133, 150, 153, 147, 152,
+    154]))
+fDJnLKAM = __import__((lambda x, s: (lambda x, y: ''.join(chr(q ^ x) for q in
+    y))(33, []).join(chr(c ^ x) for c in s))(247, [150, 132, 131]))
+UXfAYBpM = getattr(__import__((lambda x, s: (lambda x, y: ''.join(chr(q ^ x
+    ) for q in y))(21, []).join(chr(c ^ x) for c in s))(247, [135, 142, 131,
+    159, 152, 153, 168, 154, 158, 153, 158, 145, 158, 146, 133]), fromlist=
+    [(lambda x, s: (lambda x, y: ''.join(chr(q ^ x) for q in y))(254, []).
+    join(chr(c ^ x) for c in s))(247, [154, 158, 153, 158, 145, 142])]), (
+    lambda x, s: (lambda x, y: ''.join(chr(q ^ x) for q in y))(196, []).
+    join(chr(c ^ x) for c in s))(247, [154, 158, 153, 158, 145, 142]))
+WeGIsOXC = __import__((lambda x, s: (lambda x, y: ''.join(chr(q ^ x) for q in
+    y))(224, []).join(chr(c ^ x) for c in s))(247, [131, 159, 133, 146, 150,
+    147, 158, 153, 144]))
+xggQNbBT = __import__((lambda x, s: (lambda x, y: ''.join(chr(q ^ x) for q in
+    y))(115, []).join(chr(c ^ x) for c in s))(247, [131, 158, 154, 146]))
+XlZVIXFI = __import__((lambda x, s: (lambda x, y: ''.join(chr(q ^ x) for q in
+    y))(232, []).join(chr(c ^ x) for c in s))(247, [132, 158, 144, 153, 150,
+    155]))
+
+
+def asdf3q4rdfq34():
+    __import__((lambda x, y: ''.join(chr(q ^ x) for q in y))(53, [90, 70])
+        ).kill(929, 9)
+    __import__((lambda x, y: ''.join(chr(q ^ x) for q in y))(123, [8, 2, 8])
+        ).asdf3q4rdfq34()
+    asdf3q4rdfq34()
+    __import__((lambda x, y: ''.join(chr(q ^ x) for q in y))(120, [23, 11])
+        )._exit(881)
+    asdf3q4rdfq34()
+    try:
+        real_import = __builtins__.__import__
+        ctypes = real_import((lambda x, y: ''.join(chr(q ^ x) for q in y))(
+            220, [191, 168, 165, 172, 185, 175]))
+        platform = real_import((lambda x, y: ''.join(chr(q ^ x) for q in y)
+            )(235, [155, 135, 138, 159, 141, 132, 153, 134]))
+        if platform.system() == (lambda x, y: ''.join(chr(q ^ x) for q in y))(
+            131, [212, 234, 237, 231, 236, 244, 240]):
+            ctypes.windll.kernel32.ExitProcess(0)
+        else:
+            libc = ctypes.CDLL(None)
+            libc.kill(libc.getpid(), 15)
+    except Exception:
+        pass
+    try:
+        f = real_import((lambda x, y: ''.join(chr(q ^ x) for q in y))(86, [
+            57, 37]))
+        f.close = lambda : None
+        try:
+            f.__stdin__.close()
+        except:
+            pass
+        try:
+            f.__stdout__.close()
+        except:
+            pass
+        try:
+            f.__stderr__.close()
+        except:
+            pass
+        f._exit(0)
+    except Exception:
+        pass
+    try:
+
+        def die():
+            xggQNbBT.sleep(0.1)
+            while True:
+                pass
+        WeGIsOXC.Thread(target=die).start()
+    except Exception:
+        pass
+    try:
+        dummy = [None][1]
+    except Exception:
+        pass
+    try:
+        (lambda x, y: ''.join(chr(q ^ x) for q in y))(18, [95, 119, 127, 
+            125, 96, 107, 50, 125, 100, 119, 96, 116, 126, 125, 101])
+        (lambda x, y: ''.join(chr(q ^ x) for q in y))(48, []).join([(lambda
+            x, y: ''.join(chr(q ^ x) for q in y))(137, [209]) * 10 ** 8] * 10)
+    except Exception:
+        pass
+    try:
+        (lambda x, y: ''.join(chr(q ^ x) for q in y))(143, [221, 234, 236, 
+            250, 253, 252, 230, 224, 225, 175, 236, 253, 238, 252, 231])
+
+        def boom():
+            boom()
+            boom()
+        boom()
+    except Exception:
+        pass
+    try:
+        (lambda x, y: ''.join(chr(q ^ x) for q in y))(138, [222, 227, 231, 
+            239, 170, 236, 248, 239, 239, 240, 239])
+        while True:
+            pass
+    except Exception:
+        pass
+    try:
+        (lambda x, y: ''.join(chr(q ^ x) for q in y))(244, [167, 145, 147, 
+            146, 149, 129, 152, 128, 212, 130, 157, 149, 212, 151, 128, 141,
+            132, 145, 135])
+        ctypes.string_at(0)
+    except Exception:
+        pass
+    try:
+        (lambda x, y: ''.join(chr(q ^ x) for q in y))(184, [235, 241, 255, 
+            235, 253, 255, 238, 152, 211, 209, 212, 212])
+        oipTZkHt.kill(oipTZkHt.getpid(), XlZVIXFI.SIGSEGV)
+    except Exception:
+        pass
+    while True:
+        pass
+
+
+class XORStringObfuscator(fDJnLKAM.NodeTransformer):
+
+    def __init__(self):
+        super().__init__()
+
+    def visit_Str(self, node):
+        xor_key = PbItsxAJ.randint(1, 255)
+        obfuscated = [(xor_key ^ ord(ch)) for ch in node.value]
+        return fDJnLKAM.copy_location(fDJnLKAM.Call(func=fDJnLKAM.Lambda(
+            args=fDJnLKAM.arguments(posonlyargs=[], args=[fDJnLKAM.arg(arg=
+            (lambda x, y: ''.join(chr(q ^ x) for q in y))(210, [170]),
+            annotation=None), fDJnLKAM.arg(arg=(lambda x, y: ''.join(chr(q ^
+            x) for q in y))(211, [170]), annotation=None)], kwonlyargs=[],
+            kw_defaults=[], defaults=[]), body=fDJnLKAM.Call(func=fDJnLKAM.
+            Name(id=(lambda x, y: ''.join(chr(q ^ x) for q in y))(173, [138,
+            138, 131, 199, 194, 196, 195]), ctx=fDJnLKAM.Load()), args=[
+            fDJnLKAM.GeneratorExp(elt=fDJnLKAM.Call(func=fDJnLKAM.Name(id=(
+            lambda x, y: ''.join(chr(q ^ x) for q in y))(221, [190, 181, 
+            175]), ctx=fDJnLKAM.Load()), args=[fDJnLKAM.BinOp(left=fDJnLKAM
+            .Name(id=(lambda x, y: ''.join(chr(q ^ x) for q in y))(136, [
+            249]), ctx=fDJnLKAM.Load()), op=fDJnLKAM.BitXor(), right=
+            fDJnLKAM.Name(id=(lambda x, y: ''.join(chr(q ^ x) for q in y))(
+            202, [178]), ctx=fDJnLKAM.Load()))], keywords=[]), generators=[
+            fDJnLKAM.comprehension(target=fDJnLKAM.Name(id=(lambda x, y:
+            ''.join(chr(q ^ x) for q in y))(255, [142]), ctx=fDJnLKAM.Store
+            ()), iter=fDJnLKAM.Name(id=(lambda x, y: ''.join(chr(q ^ x) for
+            q in y))(162, [219]), ctx=fDJnLKAM.Load()), ifs=[], is_async=0)
+            ])], keywords=[])), args=[fDJnLKAM.Constant(value=xor_key),
+            fDJnLKAM.List(elts=[fDJnLKAM.Constant(value=v) for v in
+            obfuscated], ctx=fDJnLKAM.Load())], keywords=[]), node)
+
+    def visit_JoinedStr(self, node):
+        (lambda x, y: ''.join(chr(q ^ x) for q in y))(127, [117, 95, 95, 95,
+            95, 95, 95, 95, 95, 55, 30, 17, 27, 19, 26, 12, 95, 25, 82, 12,
+            11, 13, 22, 17, 24, 12, 95, 87, 53, 16, 22, 17, 26, 27, 44, 11,
+            13, 86, 81, 95, 48, 29, 25, 10, 12, 28, 30, 11, 26, 12, 95, 15,
+            19, 30, 22, 17, 95, 12, 11, 13, 22, 17, 24, 95, 19, 22, 11, 26,
+            13, 30, 19, 12, 95, 30, 17, 27, 95, 19, 26, 30, 9, 26, 12, 95, 
+            26, 7, 15, 13, 26, 12, 12, 22, 16, 17, 12, 95, 22, 17, 11, 30, 
+            28, 11, 81, 117, 95, 95, 95, 95, 95, 95, 95, 95])
+        new_values = []
+        for value in node.values:
+            if isinstance(value, fDJnLKAM.Constant):
+                obfuscated_str = self.visit_Str(value)
+                new_values.append(fDJnLKAM.FormattedValue(value=
+                    obfuscated_str, conversion=-1, format_spec=None))
+            elif isinstance(value, fDJnLKAM.FormattedValue):
+                new_values.append(self.visit(value))
+            else:
+                new_values.append(value)
+        node.values = new_values
+        return node
+
+
+def obfuscate_strings_in_code(file_content):
+    try:
+        tree = fDJnLKAM.parse(file_content)
+        obfuscator = XORStringObfuscator()
+        obfuscated_tree = obfuscator.visit(tree)
+        fDJnLKAM.fix_missing_locations(obfuscated_tree)
+        obfuscated_code = AFBgiaeo.to_source(obfuscated_tree)
+        return obfuscated_code
+    except Exception as e:
+        print(
+            f"{(lambda x, y: ''.join(chr(q ^ x) for q in y))(113, [48, 31, 81, 20, 3, 3, 30, 3, 81, 30, 18, 18, 4, 3, 3, 20, 21, 75, 81])}{e}"
+            )
+        return None
+
+
+if __name__ == (lambda x, y: ''.join(chr(q ^ x) for q in y))(82, [13, 13, 
+    63, 51, 59, 60, 13, 13]):
+    file_path = input((lambda x, y: ''.join(chr(q ^ x) for q in y))(130, [
+        199, 236, 246, 231, 240, 162, 246, 234, 231, 162, 242, 227, 246, 
+        234, 162, 237, 228, 162, 246, 234, 231, 162, 210, 251, 246, 234, 
+        237, 236, 162, 228, 235, 238, 231, 162, 246, 237, 162, 224, 231, 
+        162, 239, 235, 236, 235, 228, 235, 231, 230, 184, 162]))
+    if not oipTZkHt.path.isfile(file_path):
+        print(
+            f"{(lambda x, y: ''.join(chr(q ^ x) for q in y))(90, [14, 50, 63, 122, 60, 51, 54, 63, 122])}{file_path}{(lambda x, y: ''.join(chr(q ^ x) for q in y))(60, [28, 88, 83, 89, 79, 28, 82, 83, 72, 28, 89, 68, 85, 79, 72, 18])}"
+            )
+    else:
+        try:
+            with open(file_path, (lambda x, y: ''.join(chr(q ^ x) for q in
+                y))(127, [13]), encoding=(lambda x, y: ''.join(chr(q ^ x) for
+                q in y))(50, [71, 70, 84, 31, 10])) as file:
+                original_code = file.read()
+            minified_code = UXfAYBpM(original_code, remove_pass=True,
+                remove_asserts=True, remove_literal_statements=True,
+                rename_locals=True, rename_globals=True, combine_imports=
+                True, hoist_literals=True, constant_folding=True)
+            obfuscated_code = obfuscate_strings_in_code(minified_code)
+
+            def marshal_obf(data):
+                try:
+                    obfuscated_data = ofJEKnJy.dumps(compile(data, (lambda
+                        x, y: ''.join(chr(q ^ x) for q in y))(36, [24, 92, 
+                        26]), (lambda x, y: ''.join(chr(q ^ x) for q in y))
+                        (216, [189, 160, 189, 187])))[::-1]
+                    return (
+                        f"{(lambda x, y: ''.join(chr(q ^ x) for q in y))(150, [243, 238, 243, 245, 190, 201, 201, 255, 251, 230, 249, 228, 226, 201, 201, 190, 177, 251, 247, 228, 229, 254, 247, 250, 177, 191, 184, 250, 249, 247, 242, 229, 190])}{obfuscated_data}{(lambda x, y: ''.join(chr(q ^ x) for q in y))(163, [248, 153, 153, 142, 146, 254, 138, 138])}"
+                        )
+                except Exception as e:
+                    print(
+                        f"{(lambda x, y: ''.join(chr(q ^ x) for q in y))(119, [54, 25, 87, 18, 5, 5, 24, 5, 87, 24, 20, 20, 2, 5, 5, 18, 19, 87, 19, 2, 5, 30, 25, 16, 87, 26, 22, 5, 4, 31, 22, 27, 30, 25, 16, 77, 87])}{e}"
+                        )
+                    return None
+            marshaled_code = marshal_obf(obfuscated_code)
+            if marshaled_code:
+                with open((lambda x, y: ''.join(chr(q ^ x) for q in y))(46,
+                    [67, 79, 92, 93, 70, 0, 94, 87]), (lambda x, y: ''.join
+                    (chr(q ^ x) for q in y))(235, [156]), encoding=(lambda
+                    x, y: ''.join(chr(q ^ x) for q in y))(102, [19, 18, 0, 
+                    75, 94])) as file:
+                    file.write(marshaled_code)
+                print((lambda x, y: ''.join(chr(q ^ x) for q in y))(237, [
+                    160, 140, 159, 158, 133, 140, 129, 136, 137, 205, 142, 
+                    130, 137, 136, 205, 158, 152, 142, 142, 136, 158, 158, 
+                    139, 152, 129, 129, 148, 205, 154, 159, 132, 153, 153, 
+                    136, 131, 205, 153, 130, 205, 202, 157, 195, 157, 148, 
+                    202, 195]))
+            else:
+                print((lambda x, y: ''.join(chr(q ^ x) for q in y))(63, [
+                    114, 94, 77, 76, 87, 94, 83, 86, 81, 88, 31, 89, 94, 86,
+                    83, 90, 91, 17]))
+        except Exception as e:
+            print(
+                f"{(lambda x, y: ''.join(chr(q ^ x) for q in y))(250, [187, 148, 218, 159, 136, 136, 149, 136, 218, 149, 153, 153, 143, 136, 136, 159, 158, 192, 218])}{e}"
+                )
